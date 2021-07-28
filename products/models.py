fc4e32b6e2c2fde4a10ddac93fc87f93e3ceae37
bobby-22 <nlongn22@tuta.io>
2021-07-29 00:04:38 +0200

    Provide default (empty) values for price and description

--- products/models.py
+++ products/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class ProductModel(models.Model):
     name = models.CharField(max_length=100)
-    price = models.IntegerField()
-    description = models.TextField()
+    price = models.IntegerField(default="")
+    description = models.TextField(default="")
 
     def __str__(self):
         return self.name, self.description, self.price
