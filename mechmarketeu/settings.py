2d619c13565cc25f63672077c71066b971bf4047
bobby-22 <nlongn22@icloud.com>
2021-09-19 15:18:32 +0200

    fix

--- mechmarketeu/settings.py
+++ mechmarketeu/settings.py
@@ -78,7 +78,7 @@ SIMPLE_JWT = {
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080", "https://mechmarketeu.herokuapp.com/"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:8080", "http://127.0.0.1:8080", "mechmarketeu.herokuapp.com/"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
