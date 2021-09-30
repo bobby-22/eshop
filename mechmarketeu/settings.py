ec1a4efcbc141b1156224f1d28f18f7d9ab4d5ca
bobby-22 <nlongn22@icloud.com>
2021-10-01 00:19:58 +0200

    Fix

--- mechmarketeu/settings.py
+++ mechmarketeu/settings.py
@@ -76,6 +76,7 @@ REST_SESSION_LOGIN = False
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = "jwt-access-token"
 JWT_AUTH_REFRESH_COOKIE = "jwt-refresh-token"
+JWT_AUTH_SECURE = True
 
 from datetime import timedelta
 
